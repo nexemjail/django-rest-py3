032bebd1914fdc0e0b9f1f9e7de88ce2c1d1782f
nexemjail <nexemjail@gmail.com>
2016-12-30 14:42:04 +0300

    resolved issue

--- djangorest/urls.py
+++ djangorest/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
-from djangorest.api import urls as api_urls
+from api import urls as api_urls
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
